a999295762f602dbf5488928203b9bc9e4e3ebcf
JashiRL <j,roblesleconaqugto.mx>
2023-02-09 10:17:37 -0600

    Ya terminamos el proyecto

---
+++ js/data.js
@@ -0,0 +1,36 @@
+const songs = [
+    {
+        name: 'Nois',
+        path: '../music/Song 1.mp3',
+        artist: 'Los pirruiris',
+        cover: '../images/cover 1.png'
+    },
+
+    {
+        name: 'Quevedo',
+        path: '../music/Song 2.mp3',
+        artist: 'Bizarrap',
+        cover: '../images/cover 2.png'
+    },
+
+    {
+        name: 'Chocolate y Nata',
+        path: '../music/Song 3.mp3',
+        artist: 'Carlos Sadness',
+        cover: '../images/cover 3.png'
+    },
+
+    {
+        name: 'Decisions',
+        path: '../music/Song 4.mp3',
+        artist: 'KREAM',
+        cover: '../images/cover 4.png'
+    },
+
+    {
+        name: 'Mi amor por ti',
+        path: '../music/Song 5.mp3',
+        artist: 'De mi pa ti',
+        cover: '../images/cover 5.png'
+    }
+]
